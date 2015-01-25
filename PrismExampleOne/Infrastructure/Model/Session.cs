ffcfcbc422d4459ae29764a7c4b2ca8ec198dc18
barkley.hughes <barkley.hughes@gmail.com>
2015-01-24 21:20:58 -0600

    inital to new online repo

---
+++ PrismExampleOne/Infrastructure/Model/Session.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Mvvm;
+using Microsoft.Practices.Prism;
+using Microsoft.Practices.Unity;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Prism.Commands;
+
+namespace OGV.Infrastructure.Model
+{
+    public class Session : BindableBase
+    {
+        public string Token { get; set; }
+
+        public static bool Recording { get; set; }
+
+        private static readonly Session instance = new Session();
+
+        private Session() { }
+
+        public static Session Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+
+}
